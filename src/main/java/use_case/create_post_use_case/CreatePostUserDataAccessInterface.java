fd98d2c393f09292088ab2f3e1ba98d0adb07cac
anton <antonmcassidy@gmail.com>
2025-11-11 18:15:16 -0500

    UserDataAccessInteface Part1

--- src/main/java/use_case/create_post_use_case/CreatePostUserDataAccessInterface.java
+++ src/main/java/use_case/create_post_use_case/CreatePostUserDataAccessInterface.java
@@ -1,4 +1,19 @@
 package use_case.create_post_use_case;
 
 public interface CreatePostUserDataAccessInterface {
+
+    // Checks for existence of posts with the name indicated in their title.
+    Boolean existswithname(String name);
+
+    // Saves a post to database.
+    void save(Post post);
+
+    // Fetches a specific post.
+    Post getPost(String title);
+
+    //Get all viewable Posts.
+    Post<> getAllPosts();
+
+    //Get all Posts with keyword in title
+    Post<> getSelectPosts(String keyword);
 }

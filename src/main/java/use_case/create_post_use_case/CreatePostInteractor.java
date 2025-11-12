17b120659b5f680391bfb2a6d254e04f3cfd1d2d
anton <antonmcassidy@gmail.com>
2025-11-11 19:16:00 -0500

    Most of Input and Output data, the Boundaries, and data access interface implemented in use_case.create_post_use_case. TODO: Interactor, getter/setter methods in boundary, everything outside including view/model, etc.

--- src/main/java/use_case/create_post_use_case/CreatePostInteractor.java
+++ src/main/java/use_case/create_post_use_case/CreatePostInteractor.java
@@ -1,4 +1,18 @@
 package use_case.create_post_use_case;
 
-public class CreatePostInteractor {
+public class CreatePostInteractor implements CreatePostInputBoundary{
+    private final CreatePostUserDataAccessInterface createPostUserDataAccessInterface;
+    private final CreatePostOutputBoundary createPostOutputBoundary;
+
+    public CreatePostInteractor(
+            CreatePostUserDataAccessInterface createPostUserDataAccessInterface,
+            CreatePostOutputBoundary createPostOutputBoundary) {
+        this.createPostUserDataAccessInterface = createPostUserDataAccessInterface;
+        this.createPostOutputBoundary = createPostOutputBoundary;
+    }
+
+    @Override
+    public void execute(CreatePostInputData createPostInputData) {
+
+    }
 }
--- src/main/java/use_case/create_post_use_case/CreatePostOutputBoundary.java
+++ src/main/java/use_case/create_post_use_case/CreatePostOutputBoundary.java
@@ -1,4 +1,8 @@
 package use_case.create_post_use_case;
 
 public interface CreatePostOutputBoundary {
+
+    void prepareCreatedView(CreatePostOutputData createPostOutputData);
+
+    void prepareMissingFieldView();
 }

fe7dac8105cdeb8d623ba2758f49b126ce903c4e
anton <antonmcassidy@gmail.com>
2025-11-16 17:27:37 -0500

    Started working on CreatePostView

--- src/main/java/View/CreatingPostView.java
+++ src/main/java/View/CreatingPostView.java
@@ -1,4 +1,18 @@
 package View;
 
-public class CreatingPostView {
+import interface_adapter.create_post.CreatePostViewModel;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+
+public class CreatingPostView extends JPanel implements ActionListener, PropertyChangeListener {
+    private final CreatePostViewModel createPostViewModel;
+    private final JTextField titleTextField = new  JTextField(30);
+    private final JTextArea contentTextField = new  JTextArea(10, 30);
+
+    private final JButton createPostButton = new  JButton("Create Post");
+    //private final JButton logoutButton = new  JButton("Logout");
+    //private final JButton searchButton = new  JButton("Search");
+    //private final JButton browseButton = new  JButton("Browse");
 }

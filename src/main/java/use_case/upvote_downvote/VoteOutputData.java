4a03f6cfac5a740609b702ba55d56fb10773951d
IRJN <irin.jin@autodrive.utoronto.ca>
2025-11-19 10:39:56 -0500

    Added files in use_case.upvote_downvote
    # Conflicts:
    #	src/main/java/use_case/upvote_downvote/VoteInputData.java
    #	src/main/java/use_case/upvote_downvote/VoteOutputData.java

--- src/main/java/use_case/upvote_downvote/VoteOutputData.java
+++ src/main/java/use_case/upvote_downvote/VoteOutputData.java
@@ -1,19 +1,4 @@
 package use_case.upvote_downvote;
 
 public class VoteOutputData {
-    private final int newScore;
-    private final long postId;
-
-    VoteOutputData(int newScore, long postId) {
-        this.newScore = newScore;
-        this.postId = postId;
-    }
-
-    public int getNewScore() {
-        return newScore;
-    }
-    public long getPostId() {
-        return postId;
-    }
-
 }

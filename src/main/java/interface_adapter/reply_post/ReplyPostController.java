70a8e5a5920b288fe2bf9fb5dbf6a26034b5e9b3
Minh Nguyen <peternguyenstudent123@gmail.com>
2025-11-20 15:06:59 -0500

    Finished code for ReplyPostController, although this may be changed again soon.
    Signed-off-by: Minh Nguyen <peternguyenstudent123@gmail.com>

--- src/main/java/interface_adapter/reply_post/ReplyPostController.java
+++ src/main/java/interface_adapter/reply_post/ReplyPostController.java
@@ -1,5 +1,22 @@
 package interface_adapter.reply_post;
 
+import use_case.reply_post.ReplyPostInputBoundary;
+import use_case.reply_post.ReplyPostInputData;
+
 public class ReplyPostController {
-    // TODO: Implement the controller
+    private final ReplyPostInputBoundary replyPostUseCaseInteractor;
+
+    public ReplyPostController(ReplyPostInputBoundary replyPostUseCaseInteractor) {
+        this.replyPostUseCaseInteractor = replyPostUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Reply Post Use Case
+     * @param username the username of the replier
+     * @param content the content of the reply
+     */
+    public void execute(String username, String content) {
+        final ReplyPostInputData replyPostInputData = new ReplyPostInputData(username, content);
+        replyPostUseCaseInteractor.execute(replyPostInputData);
+    }
 }

5b7e3ca7e240690ba897d9322992014ce4d36aef
IRJN <irin.jin@autodrive.utoronto.ca>
2025-11-19 22:44:27 -0500

    Fixed constructors and variables.

--- src/main/java/use_case/translate/TranslationOutputData.java
+++ src/main/java/use_case/translate/TranslationOutputData.java
@@ -6,12 +6,24 @@ package use_case.translate;
  */
 public class TranslationOutputData {
     private final String translatedText;
+    private final String targetLanguage;
+    private final String sourceLanguage;
 
-    public TranslationOutputData(String translatedText) {
+    public TranslationOutputData(String translatedText, String targetLanguage, String sourceLanguage) {
         this.translatedText = translatedText;
+        this.targetLanguage = targetLanguage;
+        this.sourceLanguage = sourceLanguage;
     }
 
     public String getTranslatedText() {
         return translatedText;
+    }
+
+    public String getTargetLanguage() {
+        return targetLanguage;
+    }
+
+    public String getSourceLanguage() {
+        return sourceLanguage;
     }
 }

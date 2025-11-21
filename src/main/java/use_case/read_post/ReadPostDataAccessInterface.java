4526515ee9f74d0fc0b0aff4fa408d117f74ea36
Fred He <fredhe56@gmail.com>
2025-11-20 19:24:34 -0500

    removed getPostByTitle

--- src/main/java/use_case/read_post/ReadPostDataAccessInterface.java
+++ src/main/java/use_case/read_post/ReadPostDataAccessInterface.java
@@ -8,13 +8,6 @@ import entities.OriginalPost;
 public interface ReadPostDataAccessInterface {
 
     /**
-     * Gets a post by its title.
-     * @param title the title of the post to retrieve
-     * @return the post with the given title, or null if not found
-     */
-    OriginalPost getPostByTitle(String title);
-
-    /**
      * Gets a post by its unique ID.
      * @param id the unique identifier of the post to retrieve
      * @return the post with the given ID, or null if not found

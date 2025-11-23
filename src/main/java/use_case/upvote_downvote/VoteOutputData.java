0caefc3aa2942d5a140d9006283e0658de42a71f
IRJN <irin.jin@autodrive.utoronto.ca>
2025-11-23 10:17:52 -0500

    Wrote InputData and OutputData.

--- src/main/java/use_case/upvote_downvote/VoteOutputData.java
+++ src/main/java/use_case/upvote_downvote/VoteOutputData.java
@@ -1,4 +1,13 @@
 package use_case.upvote_downvote;
 
 public class VoteOutputData {
+    private final int score;
+    private final long postId;
+
+    VoteOutputData(int score, long postId) {
+        this.score = score;
+        this.postId = postId;
+    }
+
+
 }

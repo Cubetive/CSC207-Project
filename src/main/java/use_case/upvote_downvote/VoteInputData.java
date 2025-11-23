c2df3c3737dad84cc189541c5af2ada9d8420bc3
IRJN <irin.jin@autodrive.utoronto.ca>
2025-11-23 13:10:32 -0500

    Added interface adapter files.

--- src/main/java/use_case/upvote_downvote/VoteInputData.java
+++ src/main/java/use_case/upvote_downvote/VoteInputData.java
@@ -1,4 +1,18 @@
 package use_case.upvote_downvote;
 
 public class VoteInputData {
+    private final boolean isUpvote;
+    private final long postId;
+
+    public VoteInputData(boolean isUpvote, long postId) {
+        this.isUpvote = isUpvote;
+        this.postId = postId;
+    }
+
+    public boolean isUpvote() {
+        return isUpvote;
+    }
+    public long getPostId() {
+        return postId;
+    }
 }
